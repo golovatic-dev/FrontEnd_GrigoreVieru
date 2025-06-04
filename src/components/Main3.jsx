18a66cc8d7a804faac60641cc5b3788bcf646d10
daniel_g <danielgolovatic.dev@gmail.com>
2025-06-04 18:42:41 +0300

    fixed hover animations on main3

--- src/components/Main3.jsx
+++ src/components/Main3.jsx
@@ -1,50 +1,50 @@
-import React, { useEffect } from 'react'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import React from 'react'
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt'
 import { Link } from 'react-router-dom'
-
 function Main3() {
-  useEffect(() => {
-    AOS.init({ duration: 1000, once: true })
-  }, [])
-
   return (
     <div className="parallax-container">
       <div
-        className="parallax-bg d-flex justify-content-center align-items-center bg-center bg-no-repeat"
+        className="parallax-bg   d-flex justify-content-center align-items-center  bg-center bg-no-repeat "
         style={{
           backgroundImage: "url('src/assets/main3_bg.png')",
         }}
-        data-aos="fade-in"
       ></div>
-      <div className="container-fluid bg-dark position-absolute text-light d-flex justify-content-center align-items-center h-100 w-100 bg-opacity-25">
-        <div className="container">
-          <div className="w-100 h-100 grid grid-cols-1 lg:grid-cols-2">
-            <div className="flex flex-col justify-center" data-aos="fade-right">
+      <div className="container-fluid bg-dark position-absolute text-light d-flex justify-content-center align-items-center h-100 w-100 bg-opacity-25 ">
+        <div className=" container ">
+          <div className="w-100 h-100 grid  grid-cols-1 lg:grid-cols-2 ">
+            <div
+              className=" flex flex-col   justify-center"
+              data-aos="fade-right"
+              data-aos-delay="0"
+            >
               <h1
-                className="fw-bold text-2xl lg:text-4xl mb-4"
+                className="fw-bold text-2xl lg:text-4xl  mb-4"
                 data-aos="fade-down"
+                data-aos-delay="100"
               >
-                Gimnaziul „Grigore Vieru”
+                {' '}
+                Gimnaziul „ Grigore Vieru ”
               </h1>
               <p
-                className="text-lg lg:text-2xl"
+                className="text-lg lg:text-2xl "
                 data-aos="fade-up"
                 data-aos-delay="200"
               >
+                {' '}
                 Gimnaziul „Grigore Vieru” oferă elevilor o educație modernă și
                 un mediu prietenos de învățare. Ne mândrim cu o comunitate
                 activă, profesori dedicați și activități extracurriculare care
                 sprijină dezvoltarea armonioasă a fiecărui copil.
               </p>
-              <span data-aos="zoom-in" data-aos-delay="400">
+              <span data-aos="fade-up" data-aos-delay="300">
                 <Link
-                  to="/aboutus"
-                  onClick={() => window.scrollTo(0, 0)}
-                  className="btn shadow-lg mt-5 fw-bold rounded-0 btn-primary"
+                  to="/college"
+                  className="btn shadow-lg  mt-5 fw-bold rounded-0 btn-primary"
                 >
-                  Află mai mult <ArrowRightAltIcon />
+                  {' '}
+                  Află mai mult
+                  <ArrowRightAltIcon />{' '}
                 </Link>
               </span>
             </div>
@@ -52,12 +52,12 @@ function Main3() {
             <div
               className="hidden lg:flex justify-center"
               data-aos="fade-left"
-              data-aos-delay="300"
+              data-aos-delay="400"
             >
               <img
                 src="src/assets/gymnasium.png"
-                alt="Imagine Gimnaziu"
-                className="shadow-lg rounded-4 w-[700px] h-[400px]"
+                alt=""
+                className="shadow-lg  rounded-4 w-[700px] h-[400px]"
               />
             </div>
           </div>
