a2b63b875bd2d0aca67c620b9d63cc84095de4ff
daniel_g <danielgolovatic.dev@gmail.com>
2025-06-04 13:11:09 +0300

    animated important info on main page

--- src/components/Main2.jsx
+++ src/components/Main2.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 function Main2() {
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true })
+  }, [])
+
   const downloadBroucher = () => {
     const anchor = document.createElement('a')
     anchor.href = 'src/assets/brouchure/Brosura_Gimnaziul_Grigore_Vieru.pdf'
@@ -12,7 +18,10 @@ function Main2() {
     <div className="pb-5 bg-gradient-to-br from-indigo-800 to-gray-800">
       <div className="container">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <div className="mt-5 flex flex-col justify-center">
+          <div
+            className="mt-5 flex flex-col justify-center"
+            data-aos="fade-right"
+          >
             <span className="text-info">Informații utile</span>
             <h1 className="font-bold text-3xl lg:text-5xl mb-4 text-white">
               Despre Gimnaziul „Grigore Vieru”
@@ -28,11 +37,16 @@ function Main2() {
             <button
               className="bg-primary p-2 lg:px-6 lg:py-3 rounded-lg text-lg font-bold mt-5 text-light"
               onClick={downloadBroucher}
+              data-aos="zoom-in"
+              data-aos-delay="200"
             >
               Descarcă broșura
             </button>
           </div>
-          <div className="lg:mt-0 mt-5 flex justify-center items-center">
+          <div
+            className="lg:mt-0 mt-5 flex justify-center items-center"
+            data-aos="fade-left"
+          >
             <a
               href="src/assets/brouchure/Brosura_Gimnaziul_Grigore_Vieru.pdf"
               download="brosura-gimnaziul-grigore-vieru.pdf"
@@ -41,6 +55,8 @@ function Main2() {
                 src="src/assets/brouchure/brouchure_preview.png"
                 className="lg:w-full rounded-lg"
                 alt="Broșura Gimnaziului Grigore Vieru"
+                data-aos="flip-left"
+                data-aos-delay="400"
               />
             </a>
           </div>
