cde6dafbacee76c0602a0358ad5f1b4a05076367
daniel_g <danielgolovatic.dev@gmail.com>
2025-06-04 13:12:11 +0300

    animated learn more section on main page

--- src/components/Main3.jsx
+++ src/components/Main3.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt'
 import { Link } from 'react-router-dom'
 
 function Main3() {
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true })
+  }, [])
+
   return (
     <div className="parallax-container">
       <div
@@ -10,21 +16,29 @@ function Main3() {
         style={{
           backgroundImage: "url('src/assets/main3_bg.png')",
         }}
+        data-aos="fade-in"
       ></div>
       <div className="container-fluid bg-dark position-absolute text-light d-flex justify-content-center align-items-center h-100 w-100 bg-opacity-25">
         <div className="container">
           <div className="w-100 h-100 grid grid-cols-1 lg:grid-cols-2">
-            <div className="flex flex-col justify-center">
-              <h1 className="fw-bold text-2xl lg:text-4xl mb-4">
+            <div className="flex flex-col justify-center" data-aos="fade-right">
+              <h1
+                className="fw-bold text-2xl lg:text-4xl mb-4"
+                data-aos="fade-down"
+              >
                 Gimnaziul „Grigore Vieru”
               </h1>
-              <p className="text-lg lg:text-2xl">
+              <p
+                className="text-lg lg:text-2xl"
+                data-aos="fade-up"
+                data-aos-delay="200"
+              >
                 Gimnaziul „Grigore Vieru” oferă elevilor o educație modernă și
                 un mediu prietenos de învățare. Ne mândrim cu o comunitate
                 activă, profesori dedicați și activități extracurriculare care
                 sprijină dezvoltarea armonioasă a fiecărui copil.
               </p>
-              <span>
+              <span data-aos="zoom-in" data-aos-delay="400">
                 <Link
                   to="/aboutus"
                   onClick={() => window.scrollTo(0, 0)}
@@ -35,7 +49,11 @@ function Main3() {
               </span>
             </div>
 
-            <div className="hidden lg:flex justify-center">
+            <div
+              className="hidden lg:flex justify-center"
+              data-aos="fade-left"
+              data-aos-delay="300"
+            >
               <img
                 src="src/assets/gymnasium.png"
                 alt="Imagine Gimnaziu"
